8e32db190c4fbb220dddf9d19096cf4e202a4db4
Denis Savitskiy <savickijdenis69@gmail.com>
2026-08-02 13:17:07 +0300

    Fix tests

--- userService/demo/src/test/java/com/dsavitskiy/userservice/AbstractIntegrationTest.java
+++ userService/demo/src/test/java/com/dsavitskiy/userservice/AbstractIntegrationTest.java
@@ -2,41 +2,33 @@ package com.dsavitskiy.userservice;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-
 
 @Testcontainers
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 public abstract class AbstractIntegrationTest {
-
 
     @Container
     static PostgreSQLContainer<?> postgres =
         new PostgreSQLContainer<>("postgres:16")
-            .withDatabaseName("Payment_System")
-            .withUsername("postgres")
-            .withPassword("postgres");
+            .withDatabaseName("testdb")
+            .withUsername("test")
+            .withPassword("test");
 
     @DynamicPropertySource
     static void configureDatabase(DynamicPropertyRegistry registry) {
-        registry.add(
-            "spring.datasource.url",
-            postgres::getJdbcUrl
-        );
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.datasource.driver-class-name", () -> "org.postgresql.Driver");
 
-        registry.add(
-            "spring.datasource.username",
-            postgres::getUsername
-        );
+        registry.add("spring.liquibase.enabled", () -> false);
 
-        registry.add(
-            "spring.datasource.password",
-            postgres::getPassword
-        );
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
     }
 }
